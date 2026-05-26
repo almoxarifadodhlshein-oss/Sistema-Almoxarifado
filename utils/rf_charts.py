66210a39479013acfb3b922634e30f3c3e5aeaaf
Arthur Mendes Sacramento (DHL Supply Chain) <arthur.mendessacramento@dhl.com>
2026-05-26 09:37:13 -0300

    feat: adiciona graficos para dashboard plotly

---
+++ utils/rf_charts.py
@@ -0,0 +1,85 @@
+import plotly.express as px
+
+
+
+# EVOLUÇÃO SEMANAL
+
+
+def grafico_evolucao(df):
+
+    fig = px.line(
+
+        df,
+
+        x="semana",
+
+        y=[
+            "disponiveis",
+            "quebrados",
+            "ausentes"
+        ],
+
+        markers=True,
+
+        title="Evolução Semanal RFs"
+    )
+
+    return fig
+
+
+# ==========================================
+# DISPONIBILIDADE POR ÁREA
+# ==========================================
+
+def grafico_area(df):
+
+    fig = px.bar(
+
+        df,
+
+        x="percentual_disponiveis",
+
+        y="area_atual",
+
+        orientation="h",
+
+        title="Disponibilidade por Área",
+
+        hover_data=[
+            "total",
+            "disponiveis",
+            "quebrados",
+            "ausentes"
+        ]
+    )
+
+    return fig
+
+
+# ==========================================
+# DISPONIBILIDADE POR MARCA
+# ==========================================
+
+def grafico_marca(df):
+
+    fig = px.bar(
+
+        df,
+
+        x="percentual_disponiveis",
+
+        y="marca",
+
+        orientation="h",
+
+        title="Disponibilidade por Marca",
+
+        hover_data=[
+            "total",
+            "disponiveis",
+            "quebrados",
+            "ausentes"
+        ]
+    )
+
+    return fig
