575ee51e1901ecb18f35f72d27dd5fbc71890a2e
Arthur Mendes Sacramento <arthur.mendessacramento@dhl.com>
2026-05-27 09:02:19 -0300

    refactor: acrécimo visual de %

--- utils/rf_analytics.py
+++ utils/rf_analytics.py
@@ -1,24 +1,42 @@
 import pandas as pd
 
 
-
+# ==========================================
 # CALCULAR PERCENTUAIS
-
+# ==========================================
 
 def calcular_percentuais(df):
 
     df = df.copy()
 
+    # EVITAR DIVISÃO POR ZERO
+    df["total"] = df["total"].replace(0, 1)
+
     df["percentual_disponiveis"] = (
-        df["disponiveis"] / df["total"] * 100
+        (df["disponiveis"] / df["total"]) * 100
     ).round(2)
 
     df["percentual_quebrados"] = (
-        df["quebrados"] / df["total"] * 100
+        (df["quebrados"] / df["total"]) * 100
     ).round(2)
 
     df["percentual_ausentes"] = (
-        df["ausentes"] / df["total"] * 100
+        (df["ausentes"] / df["total"]) * 100
     ).round(2)
+
+    df["disponiveis_%"] = (
+        df["percentual_disponiveis"]
+        .map(lambda x: f"{x:.2f}%")
+    )
+
+    df["quebrados_%"] = (
+        df["percentual_quebrados"]
+        .map(lambda x: f"{x:.2f}%")
+    )
+
+    df["ausentes_%"] = (
+        df["percentual_ausentes"]
+        .map(lambda x: f"{x:.2f}%")
+    )
 
     return df
